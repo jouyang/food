042598c55d1daf05955de21184f9141452a98477
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-01-21 14:58:56 -0800

    initial

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return 'Home page'
+
+@app.route('/data')
+def recipes():
+        data = {"recipes": ["egg","pork"],
+                "ingredients":["soy","butter"]}
+        return jsonify(data)
+
+if __name__ == '__main__':
+        app.run()
---
+++ scrape.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://allrecipes.com/Recipe/Scrumptious-Salmon-Cakes/Detail.aspx?soid=recs_recipe_1"
+
+def ingredients(url):
+	response = requests.get(url)
+
+	soup = BeautifulSoup(response.text)
+
+	ingredients_full = soup.findAll('p', attrs =  {'class':'fl-ing'})
+	ingredients = []
+	for ingred in ingredients_full:
+		pieces = ingred.findAll('span')
+		ingredients.append((pieces[0].text,pieces[1].text))
+
+	return ingredients
+
+print ingredients(url)
